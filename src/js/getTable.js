b1a3c4c80d0548e558e80ba68d9cb92d2d49ad82
Konstantin Beloglazov <ajoq@ya.ru>
2022-05-06 14:00:54 +0300

    done v2

--- src/js/getTable.js
+++ src/js/getTable.js
@@ -3,25 +3,19 @@ export default function getTable(arrData) {
 
   arrData.forEach((item) => {
     const tr = document.createElement('tr');
+
     tr.dataset.id = item.id;
     tr.dataset.title = item.title;
     tr.dataset.year = item.year;
     tr.dataset.imdb = item.imdb;
 
-    const tdId = document.createElement('td');
-    const tdtitle = document.createElement('td');
-    const tdyear = document.createElement('td');
-    const tdimdb = document.createElement('td');
+    tr.innerHTML = `
+        <td>${item.id}</td>
+        <td>${item.title}</td>
+        <td>(${item.year})</td>
+        <td>imdb: ${item.imdb.toFixed(2)}</td>
+    `;
 
-    tdId.textContent = item.id;
-    tdtitle.textContent = item.title;
-    tdyear.textContent = `(${item.year})`;
-    tdimdb.textContent = `imdb: ${item.imdb.toFixed(2)}`;
-
-    tr.append(tdId);
-    tr.append(tdtitle);
-    tr.append(tdyear);
-    tr.append(tdimdb);
     tableBody.append(tr);
   });
 }
