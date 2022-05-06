a195b3102420d54a5d3c5b1c4a458841f48e5094
Konstantin Beloglazov <ajoq@ya.ru>
2022-05-06 17:43:17 +0300

    done v3.2

--- src/js/getTable.js
+++ src/js/getTable.js
@@ -4,11 +4,6 @@ export default function getTable(arrData) {
   arrData.forEach((item) => {
     const tr = document.createElement('tr');
 
-    tr.dataset.id = item.id;
-    tr.dataset.title = item.title;
-    tr.dataset.year = item.year;
-    tr.dataset.imdb = item.imdb;
-
     tr.innerHTML = `
         <td>${item.id}</td>
         <td>${item.title}</td>
